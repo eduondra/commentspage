d9fa6ed2a9b4c2bddc55f3023b17d3953271d80f
eduondra <44001837+eduondra@users.noreply.github.com>
2020-04-16 12:47:40 +0200

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './pages/login/login.component';
+import {RegisterComponent} from './pages/register/register.component';
+import {UsersComponent} from './pages/users/users.component';
+import {UserComponent} from './pages/user/user.component';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'user', component: UserComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/config/http-config.ts
@@ -0,0 +1,3 @@
+export const httpConfig = {
+  url: 'http://85.160.64.233:3000/'
+};
